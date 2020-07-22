a4db99b6b42fd460f2898ee79e03d2afa9f0d605
Mel Jatau <meljatau@gmail.com>
2020-07-22 08:28:11 +0100

    taskbox UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './utils/redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+console.log(store);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ storybook-static/precache-manifest.a8d81900979811d3b624d8d7871339b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8992400e2f577d245be1461c04b862",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d803d17526eb1e1441c7.bundle.js"
+  },
+  {
+    "url": "runtime~main.d803d17526eb1e1441c7.bundle.js"
+  },
+  {
+    "revision": "db663e9521355dbe7690",
+    "url": "static/css/main.e9a7d208.chunk.css"
+  },
+  {
+    "url": "vendors~main.d803d17526eb1e1441c7.bundle.js"
+  },
+  {
+    "url": "vendors~main.d803d17526eb1e1441c7.bundle.js.LICENSE.txt"
+  }
+]);
